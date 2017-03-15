141e86207098d6e7e4c1be6bfcf94de9bdc8fa3b
heroTM737 <xuantien.nguyen93@gmail.com>
2017-03-15 21:40:45 +0700

    update dota2

--- app.js
+++ app.js
@@ -2,28 +2,32 @@ var http = require('http');
 var fs = require('fs');
 var request = require("request");
 var jsdom = require("jsdom");
-var credential = require("./credential/credential");
+// var credential = require("./credential/credential");
 
-var proxyUrl = "http://" + credential.username + ":" + credential.password + "@" + credential.proxy + ":" + credential.port + "/";
-var proxiedRequest = request.defaults({'proxy': proxyUrl});
+var host = "http://www.dota2.com/heroes/?l=english";
 
-var host = "https://www.google.com.vn/search?q=cat&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiS4vW52NfSAhVEv5QKHeYVBN8Q_AUICCgB&biw=1858&bih=1014";
-proxiedRequest.get(host, function(err, resp, body){
-	
-	var img_sources = jsdom.env(
-	  body,
-	  function (errors, window) {
-		var imgs = window.document.getElementsByTagName('img');
-		for (var i = 0; i < imgs.length; i++) {
-		  var src = imgs[i].getAttribute('src');
-		  if (src ){
-			  if (src.indexOf("http") == 0 || src.indexOf("https") == 0){
-				  proxiedRequest.get(src).pipe(fs.createWriteStream('downloaded/' + i + ".jpg"));
-			  }
-			  
-		  } 
-		}
-	  }
-	);
-});
+var callback = function(err, resp, body) {
+	var img_sources = jsdom.env(body, function (errors, window) {
+		var imgs = window.document.getElementsByClassName('heroHoverLarge');
+			for (var i = 0; i < imgs.length; i++) {
+				var id = imgs[i].getAttribute('id');
+				var hero_id = id.substring(id.indexOf("_") + 1);
+				console.log(hero_id);
+
+				var src = imgs[i].getAttribute('src');
+				if (src ){
+					if (src.indexOf("http") == 0 || src.indexOf("https") == 0){
+						request.get(src).pipe(fs.createWriteStream('downloaded/' + hero_id + ".jpg"));
+					}
+					
+				} 
+			}
+	});
+}
+
+// var proxyUrl = "http://" + credential.username + ":" + credential.password + "@" + credential.proxy + ":" + credential.port + "/";
+// var proxiedRequest = request.defaults({'proxy': proxyUrl});
+// proxiedRequest.get(host, callback);
+
+request.get(host, callback);
 
